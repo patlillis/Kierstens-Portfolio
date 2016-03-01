68a59f1e699ebdca4fc53032640711aee4e5ee2d
Pat Lillis <lillispm@gmail.com>
2016-02-29 22:12:46 -0500

    Make contact form actually work

--- javascript/site.js
+++ javascript/site.js
@@ -4,20 +4,24 @@
 
 $(function() {
     /*============================================
-	Resize Functions
-	==============================================*/
-    $(window).resize(onResize);
+    Resize Functions
+    ==============================================*/
+    $(window)
+        .resize(onResize);
     var prevWidth = -1;
     onResize();
-    function onResize() {
-        $('.hero').height($(window).height());
-        $('.message-box').css({
-            'marginTop': $(window).height() * 0.4
-        });
 
-        var width = window.innerWidth
-            || document.documentElement.clientWidth
-            || document.body.clientWidth;
+    function onResize() {
+        $('.hero')
+            .height($(window)
+                .height());
+        $('.message-box')
+            .css({
+                'marginTop': $(window)
+                    .height() * 0.4
+            });
+
+        var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
         var isMobile = width < 768;
         var prevIsMobile = prevWidth > 768;
@@ -29,8 +33,7 @@ $(function() {
                 offset: 65,
                 scrollOnLoad: false
             });
-        }
-        else if (!isMobile && prevIsMobile) {
+        } else if (!isMobile && prevIsMobile) {
             console.log('Switching out of  Mobile');
             smoothScroll.init({
                 offset: 78,
@@ -41,15 +44,20 @@ $(function() {
         prevWidth = width;
     }
 
-    $(window).scroll(setNavScrolled);
+    $(window)
+        .scroll(setNavScrolled);
 
     setNavScrolled();
+
     function setNavScrolled() {
         //Handle navbar
-        if ($(window).scrollTop() === 0) {
-            $('#main-navbar').removeClass('scrolled');
+        if ($(window)
+            .scrollTop() === 0) {
+            $('#main-navbar')
+                .removeClass('scrolled');
         } else {
-            $('#main-navbar').addClass('scrolled');
+            $('#main-navbar')
+                .addClass('scrolled');
         }
     }
 
@@ -76,68 +84,198 @@ $(function() {
     }];
 
     for (var i = 0; i < portfolioItemsData.length; i++) {
-        portfolioItems.eq(2 * i).add(portfolioItems.eq((2 * i) + 1)).magnificPopup({
-            disableOn: 700,
-            type: 'iframe',
-            mainClass: 'mfp-fade',
-            removalDelay: 160,
-            preloader: false,
-            closeBtnInside: true,
-            mainClass: 'my-mfp-slide-bottom',
-            fixedContentPos: true,
-            fixedBgPos: true,
-            iframe: {
-                markup: magnificPopupContent(portfolioItemsData[i].title, portfolioItemsData[i].description)
-            }
-        });
+        portfolioItems.eq(2 * i)
+            .add(portfolioItems.eq((2 * i) + 1))
+            .magnificPopup({
+                disableOn: 700,
+                type: 'iframe',
+                mainClass: 'mfp-fade',
+                removalDelay: 160,
+                preloader: false,
+                closeBtnInside: true,
+                mainClass: 'my-mfp-slide-bottom',
+                fixedContentPos: true,
+                fixedBgPos: true,
+                iframe: {
+                    markup: magnificPopupContent(portfolioItemsData[i].title, portfolioItemsData[i].description)
+                }
+            });
     }
 
     // Make sure close button works
-    $(document).on('click', '.mfp-close-custom', function() {
-        portfolioItems.magnificPopup('close');
-    });
+    $(document)
+        .on('click', '.mfp-close-custom', function() {
+            portfolioItems.magnificPopup('close');
+        });
 
     // HTML markup of popup, `mfp-close` will be replaced by the close button
     function magnificPopupContent(title, description) {
         return '<div class="mfp-iframe-scaler" style="overflow: initial;">' +
-        '<button class="mfp-close-custom"><i class="glyphicon glyphicon-remove"></i></button>' +
-        '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
-        '<h3>' + title + '</h5>' +
-        "<p>" + description + "</p>" +
-        '</div>';
+            '<button class="mfp-close-custom"><i class="glyphicon glyphicon-remove"></i></button>' +
+            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>' +
+            '<h3>' + title + '</h5>' +
+            "<p>" + description + "</p>" +
+            '</div>';
     }
 
     var timelineBlocks = $('.cd-timeline-block'),
-    offset = 0.8;
+        offset = 0.8;
 
     //hide timeline blocks which are outside the viewport
     hideBlocks(timelineBlocks, offset);
 
     //on scolling, show/animate timeline blocks when enter the viewport
-    $(window).on('scroll', function() {
-        (!window.requestAnimationFrame) ? setTimeout(function() {
-            showBlocks(timelineBlocks, offset);
-        }, 100): window.requestAnimationFrame(function() {
-            showBlocks(timelineBlocks, offset);
+    $(window)
+        .on('scroll', function() {
+            (!window.requestAnimationFrame) ? setTimeout(function() {
+                showBlocks(timelineBlocks, offset);
+            }, 100): window.requestAnimationFrame(function() {
+                showBlocks(timelineBlocks, offset);
+            });
         });
-    });
 
     function hideBlocks(blocks, offset) {
         blocks.each(function() {
-            ($(this).offset().top > $(window).scrollTop() + $(window).height() * offset) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+            ($(this)
+                .offset()
+                .top > $(window)
+                .scrollTop() + $(window)
+                .height() * offset) && $(this)
+                .find('.cd-timeline-img, .cd-timeline-content')
+                .addClass('is-hidden');
         });
     }
 
     function showBlocks(blocks, offset) {
         blocks.each(function() {
-            ($(this).offset().top <= $(window).scrollTop() + $(window).height() * offset && $(this).find('.cd-timeline-img').hasClass('is-hidden')) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+            ($(this)
+                .offset()
+                .top <= $(window)
+                .scrollTop() + $(window)
+                .height() * offset && $(this)
+                .find('.cd-timeline-img')
+                .hasClass('is-hidden')) && $(this)
+                .find('.cd-timeline-img, .cd-timeline-content')
+                .removeClass('is-hidden')
+                .addClass('bounce-in');
         });
     }
 
     //Make sure navbar closes when link is clicked
-    $(document).on('click', '.navbar-collapse.in', function(e) {
-        if ($(e.target).is('a') && ($(e.target).attr('class') != 'dropdown-toggle')) {
-            $(this).collapse('hide');
-        }
-    });
+    $(document)
+        .on('click', '.navbar-collapse.in', function(e) {
+            if ($(e.target)
+                .is('a') && ($(e.target)
+                    .attr('class') != 'dropdown-toggle')) {
+                $(this)
+                    .collapse('hide');
+            }
+        });
+
+    $('#contact-form')
+        .on('submit', function(event) {
+            var name = $('#contact-name')
+                .val(),
+                email = $('#contact-email')
+                .val(),
+                message = $('#contact-message')
+                .val(),
+                gotcha = $('#contact-gotcha')
+                .val();
+
+
+
+            var blank = [];
+
+            if (name === undefined || name === '') blank.push("name");
+            if (email === undefined || email === '') blank.push("email address");
+            if (message === undefined || message === '') blank.push("message");
+
+
+            if (blank.length > 0) {
+                var errorMsg = "Looks like you forgot to enter your ";
+
+                if (blank.length === 3) {
+                    errorMsg += blank[0] + ", " + blank[1] + ", and " + blank[2] + ".";
+                } else {
+                    errorMsg += blank.join(" and ") + ".";
+                }
+
+                swal({
+                    title: "Oops...",
+                    text: errorMsg,
+                    type: "error",
+                    confirmButtonText: "OK",
+                    allowOutsideClick: true
+                });
+            } else {
+                var url = '//formspree.io/',
+                    e = "pnjwxyjsqnqqnx%lrfnq%htr";
+                    // e = "qnqqnxur%lrfnq%htr";
+
+                url += caesarShift(e, -5).replace('%', '@').replace('%', '.');
+
+                $.ajax({
+                    url: url,
+                    data: {
+                        Name: name,
+                        Email: email,
+                        Message: message,
+                        _subject: 'New Contact Request from ' + name,
+                        _gotcha: gotcha
+                    },
+                    type: 'POST',
+                    dataType: "json",
+                    success: function(result) {
+                        swal({
+                            title: "Thanks!",
+                            text: "Your message has been sent. I'll get back to your as soon as I can!",
+                            type: "success",
+                            confirmButtonText: "OK",
+                            allowOutsideClick: true
+                        });
+                    }
+                });
+            }
+
+            event.preventDefault();
+            event.stopPropagation();
+            return false;
+        });
 });
+
+
+/* 
+ * Caesar cipher
+ * 
+ * Copyright (c) 2015 Project Nayuki
+ * All rights reserved. Contact Nayuki for licensing.
+ * https://www.nayuki.io/page/caesar-cipher-javascript
+ */
+
+"use strict";
+
+
+
+/*
+ * Returns the result of having each alphabetic letter of the given text string shifted forward
+ * by the given amount, with wraparound. Case is preserved, and non-letters are unchanged.
+ * Examples:
+ *   caesarShift("abz",  0) = "abz"
+ *   caesarShift("abz",  1) = "bca"
+ *   caesarShift("abz", 25) = "zay"
+ *   caesarShift("THe 123 !@#$", 13) = "GUr 123 !@#$"
+ */
+function caesarShift(text, shift) {
+    var result = "";
+    for (var i = 0; i < text.length; i++) {
+        var c = text.charCodeAt(i);
+        if (c >= 65 && c <= 90)
+            result += String.fromCharCode((c - 65 + shift) % 26 + 65); // Uppercase
+        else if (c >= 97 && c <= 122)
+            result += String.fromCharCode((c - 97 + shift) % 26 + 97); // Lowercase
+        else
+            result += text.charAt(i); // Copy
+    }
+    return result;
+}
